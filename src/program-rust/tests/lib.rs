7b76cad7b36303a0aaed3c029c6bb5a468b7a39c
Maksim Vykhota <m.vykhota@protonmail.com>
2021-08-05 16:13:29 +0300

    add players to tests

--- src/program-rust/tests/lib.rs
+++ src/program-rust/tests/lib.rs
@@ -5,17 +5,15 @@ use solana_sdk::{
     account::Account,
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
-    signature::Signer,
+    signature::{Signer, Keypair},
     transaction::Transaction,
 };
-use std::mem;
 
 #[tokio::test]
 async fn test_helloworld() {
     let program_id = Pubkey::new_unique();
     
-    let player_one = Pubkey::new_unique();
-    let player_two = Pubkey::new_unique();
+    let game_account_pubkey = Pubkey::new_unique();
 
     let mut program_test = ProgramTest::new(
         "tic-tac-toe", // Run the BPF version with `cargo test-bpf`
@@ -24,89 +22,92 @@ async fn test_helloworld() {
     );
 
     program_test.add_account(
-        player_one,
+        game_account_pubkey,
         Account {
             lamports: 50000,
-            data: vec![0_u8; mem::size_of::<GameState>()],
+            data: vec![0_u8; 10], // fixme: wrong size
             owner: program_id,
             ..Account::default()
         },
     );
 
-    program_test.add_account(
-        player_two,
-        Account {
-            lamports: 50000,
-            data: vec![0_u8; mem::size_of::<GameState>()],
-            owner: program_id,
-            ..Account::default()
-        },
-    );
+    let player_one = Keypair::new();
+    let player_two = Keypair::new();
 
-    let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
+    for player in [&player_one, &player_two] {
+        program_test.add_account(
+            player.pubkey(),
+            Account {
+                lamports: 50000,
+                ..Account::default()
+            },
+        );
+    }
 
-    // Verify account has zero greetings
+    let (mut banks_client, _, recent_blockhash) = program_test.start().await;
+
+    // Game field is empty
     let tic_tac_account = banks_client
-        .get_account(player_one)
+        .get_account(game_account_pubkey)
         .await
         .expect("get_account")
-        .expect("greeted_account not found");
-    
+        .expect("game_account not found");
+
     assert_eq!(
-        GameState::try_from_slice(&tic_tac_account.data)
-            .unwrap()
-            .play_field[0],
-        0
+        GameState::try_from_slice(&tic_tac_account.data).unwrap().play_field,
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
     );
 
-    // Greet once
+    // Make move
     let mut transaction = Transaction::new_with_payer(
         &[Instruction::new_with_bincode(
             program_id,
-            &[1u8, 0, 0], // ignored but makes the instruction unique in the slot
-            vec![AccountMeta::new(player_one, false)],
+            &[1u8, 0, 0],
+            vec![
+                AccountMeta::new(game_account_pubkey, false),
+                AccountMeta::new(player_one.pubkey(), true)
+            ],
         )],
-        Some(&payer.pubkey()),
+        Some(&player_one.pubkey()),
     );
-    transaction.sign(&[&payer], recent_blockhash);
+    transaction.sign(&[&player_one], recent_blockhash);
     banks_client.process_transaction(transaction).await.unwrap();
 
-    // Verify account has one greeting
-    let greeted_account = banks_client
-        .get_account(player_one)
+    // Verify first player made his turn
+    let game_account = banks_client
+        .get_account(game_account_pubkey)
         .await
         .expect("get_account")
-        .expect("greeted_account not found");
+        .expect("game_account not found");
 
     assert_eq!(
-        GameState::try_from_slice(&greeted_account.data)
-            .unwrap()
-            .play_field[0],
-        1
+        GameState::try_from_slice(&game_account.data).unwrap().play_field,
+        [1, 0, 0, 0, 0, 0, 0, 0, 0]
     );
 
-    // Greet again
+    // Make second turn
     let mut transaction = Transaction::new_with_payer(
         &[Instruction::new_with_bincode(
             program_id,
             &[1u8, 0, 1], // ignored but makes the instruction unique in the slot
-            vec![AccountMeta::new(player_two, false)],
+            vec![
+                AccountMeta::new(game_account_pubkey, false),
+                AccountMeta::new(player_two.pubkey(), true),
+            ],
         )],
-        Some(&payer.pubkey()),
+        Some(&player_two.pubkey()),
     );
-    transaction.sign(&[&payer], recent_blockhash);
+    transaction.sign(&[&player_two], recent_blockhash);
     banks_client.process_transaction(transaction).await.unwrap();
 
-    // Verify account has two greetings
-    let greeted_account = banks_client
-        .get_account(player_one)
+    // Verify second player made his turn
+    let game_account = banks_client
+        .get_account(game_account_pubkey)
         .await
         .expect("get_account")
-        .expect("greeted_account not found");
+        .expect("game_account not found");
     assert_eq!(
-        GameState::try_from_slice(&greeted_account.data)
-            .unwrap()
-            .play_field[1],
-        2
+        GameState::try_from_slice(&game_account.data).unwrap().play_field,
+        [1, 2, 0, 0, 0, 0, 0, 0, 0]
     );
 }
